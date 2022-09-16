929f53361d347d700fdda254551f86a1be1d7b5c
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-09-16 13:43:41 -0300

    map

---
+++ Arrays/map.js
@@ -0,0 +1,34 @@
+//? O método map() invoca a função callback passada
+//? por argumento para cada elemento do Array e devolve
+//? um novo Array como resultado.
+
+// var numbers = [1, 4, 9]
+// var roots = numbers.map(Math.sqrt)
+
+// //? Math.sqrt()
+// //? A função Math.sqrt() retorna a raiz quadrada de um número
+
+// console.log(roots)
+// //! (3) [1, 2, 3]
+
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*     | multiplicando usando o map |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// var numbers = [1, 4, 9]
+// var doubles = numbers.map(function (num) {
+//   return num * 2
+// })
+
+// console.log(doubles)
+// //! (3) [2, 8, 18]
+// //? numbers ainda é [1, 4, 9]
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*     | Usando map para inverter uma string |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// var str = '12345';
+// const result = [].map.call(str, function(x) {
+//   return x
+// }).reverse().join('')
+
+// console.log(result)
+//! 54321
