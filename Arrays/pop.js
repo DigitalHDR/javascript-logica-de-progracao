94eed7132d1e66d0a9f6afdaa187442823bbe664
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-09-16 14:01:56 -0300

    pop

---
+++ Arrays/pop.js
@@ -0,0 +1,18 @@
+//? O método pop() remove o último elemento
+//? de um array e retorna aquele elemento.
+
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*     | Removendo o último elemento de um array |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// var meuPeixe = ['acara-bandeira', 'palhaco', 'mandarim', 'esturjao']
+// console.log(meuPeixe)
+// //! (4) ['acara-bandeira', 'palhaco', 'mandarim', 'esturjao']
+
+// var meuPeixePop = meuPeixe.pop()
+
+// console.log(meuPeixePop)
+// //! esturjao
+// //? foi removido e retorna ele
+
+// console.log(meuPeixe)
+// //! (3) ['acara-bandeira', 'palhaco', 'mandarim']
