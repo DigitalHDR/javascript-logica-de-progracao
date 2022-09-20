adb27baf0b5b6c0cfc59e8ed84d1408c0402e29b
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-09-20 13:30:02 -0300

    unshift

---
+++ Arrays/toString.js
@@ -0,0 +1,10 @@
+//? O método toString() retorna uma string representando
+//? um array específico e seus elementos.
+
+var monthNames = ['Jan', 'Feb', 'Mar', 'Apr']
+var myVar = monthNames.toString()
+console.log(myVar)
+//! Jan,Feb,Mar,Apr
+
+console.log(monthNames)
+//! (4) ['Jan', 'Feb', 'Mar', 'Apr']
