58c3a3d18eacc718a2ff0fcaa74b8cc6f79a08a7
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-11-08 10:51:49 -0300

    toString

---
+++ String/toString.js
@@ -0,0 +1,10 @@
+//? O método toString() retorna uma string
+//? representando o objeto especificado.
+
+//? Sintaxe
+// str.toString()
+
+var x = new String('Hello world')
+
+console.log(x.toString())
+//! retorna 'Hello world'
