b260a3c8d4b72c213896630364067ec2c95535dc
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-13 13:20:07 -0300

    indexOf Sintaxe Parametros

--- Arrays/indexof.js
+++ Arrays/indexof.js
@@ -2,6 +2,24 @@
 //? que o elemento pode ser encontrado no array, retorna -1
 //? caso o mesmo não esteja presente.
 
+//? Sintaxe
+// array.indexOf(elementoDePesquisa, [pontoInicial = 0])
+
+//? Parâmetros
+//? elementoDePesquisa
+// Elemento a ser pesquisado no array.
+
+//? pontoInicial
+// O índice para iniciar a procura. Se o índice for maior ou igual ao
+// tamanho do array, é retornado -1 e signfica que o item não será
+// procurado. Se o pontoInicial é fornecido com um número negativo,
+// é tomado como deslocamento da extremidade do array.
+// Nota: se o pontoInicial fornecido é negativo, a procura no array
+// acontece de frente para trás. Se o pontoInicial fornecido é 0,
+// então o array inteiro será pesquisado. Padrão: 0 (pesquisa em todo array)
+
+//todo =====================================================
+
 // var array = [2, 5, 9]
 // var result1 = array.indexOf(2)
 // var result2 = array.indexOf(7);     // -1
