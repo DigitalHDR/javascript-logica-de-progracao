5b31d82374483836e00162e9c507fcbf620d427b
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-13 13:42:56 -0300

    lastIndexOf Sintaxe Parametros

--- Arrays/lastindexof.js
+++ Arrays/lastindexof.js
@@ -3,6 +3,24 @@
 //? ou -1 se o elemento não estiver presente. O array
 //? é pesquisado de trás para frente, começando pelo fromIndex.
 
+//?Sintaxe
+// arr.lastIndexOf(searchElement[, fromIndex = arr.length - 1])
+
+//? Parâmetros
+//? searchElement
+// Elemento para ser localizado no array.
+
+//? fromIndex
+// Opcional. O índice ao qual a busca será iniciada de traz para frente.
+// O valor padrão é o tamanho total do array menos um
+// (array.length -1), ou seja, todo o array será pesquisado.
+// Se o índice for maior ou igual ao tamanho do array, o array inteiro
+// será pesquisado. Se for negativo, ele é tomado como deslocamento
+// no final do array. Note que mesmo se o índice for negativo, o array
+// ainda será pesquisado de traz para frente. Se o índice calculado
+// for menor que 0, -1 será retornado, ou seja, o array não será pesquisado.
+
+
 // var array = [2, 5, 9, 2]
 // var result = array.lastIndexOf(2)
 // console.log(result)
