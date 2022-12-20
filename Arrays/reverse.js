3adc7292f397d0663e73246da22287c939cfaece
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-20 13:53:27 -0300

    reverse sintaxe

--- Arrays/reverse.js
+++ Arrays/reverse.js
@@ -2,6 +2,11 @@
 //? um array. O primeiro elemento do array se
 //? torna o último e o último torna-se o primeiro.
 
+//? Sintaxe
+// arr.reverse()
+
+//todo =====================================================
+
 var myArray = ['one', 'two', 'three', ' 4', '5', '6', '7', '8', '9']
 const result1 = myArray.reverse()
 console.log(result1)
