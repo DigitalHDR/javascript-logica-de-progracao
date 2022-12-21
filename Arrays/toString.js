c8ebe2a4f52d4fa0218bc2c2e9ab28124499df1d
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-21 00:59:45 -0300

    toString sintaxe e paremetro

--- Arrays/toString.js
+++ Arrays/toString.js
@@ -1,6 +1,14 @@
 //? O método toString() retorna uma string representando
 //? um array específico e seus elementos.
 
+//? Sintaxe
+// numObj.toString([radix])
+
+//? Parâmetros
+// radix
+// Opcional. Um inteiro entre 2 e 36 especificando a base utilizada
+// para representar os valores numéricos.
+
 var monthNames = ['Jan', 'Feb', 'Mar', 'Apr']
 var myVar = monthNames.toString()
 console.log(myVar)
